6494ee33297fc21dc29c8fd07f9022281335cbbd
Black Cherry <bkcherry11@gmail.com>
2013-04-23 14:20:41 -0300

    Update 002.c

--- 002/C/002.c
+++ 002/C/002.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
-// 1 1 [2] 3 5 [8] 13 21 [34] 55 89 [144] 233 377 [610] ...
+// 1 1 [2] 3 5 [8] 13 21 [34] ...
+// a b [c] a b [c] a b [c] ...
 
 int main()
 {
-	int a, b, c;
-	a = 1;
-	b = 1;
-	c = 0;
-	c = a + b;
-
-	while (c <= 4000000) {
-		a = c + b;
-		b = a + c;
-		c = a + b;
-
-		if (c < 4000000) {
-			printf("%d \n", c);	
+	int a=1, b=1, c, d=0;
+	while (c < 4000000){
+		c = a + b; 
+		a = c + b; 
+		b = a + c; 
+	if (c < 4000000){
+		d += c;
 		}
 	}
+	printf("d: %d\n", d);	
 }
