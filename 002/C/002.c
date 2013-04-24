3f47cf3407fcd1e62184945b1aa4fc2f45a57646
Dany <daniela@pistach.es>
2013-04-24 14:46:59 -0300

    solution 004

--- 002/C/002.c
+++ 002/C/002.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
-// 1 1 [2] 3 5 [8] 13 21 [34] 55 89 [144] 233 377 [610] ...
+// 1 1 [2] 3 5 [8] 13 21 [34] ...
+// a b [c] a b [c] a b [c] ...
 
 int main()
 {
-<<<<<<< HEAD
-	int a, b, c;
-=======
- int a, b, c;
->>>>>>> 0f5ed305be53fd392ca629d2a1c31dead75796b7
-	a = 1;
-	b = 1;
-	c = 0;
-	c = a + b;
-
-	while (c <= 4000000) {
-		a = c + b;
-		b = a + c;
-		c = a + b;
-
-		if (c < 4000000) {
-			printf("%d \n", c);	
+	int a=1, b=1, c, d=0;
+	while (c < 4000000){
+		c = a + b; // 2
+		a = c + b; // 3
+		b = a + c; // 5
+	if (c < 4000000){
+		d += c;
 		}
 	}
-<<<<<<< HEAD
+	printf("d: %d\n", d);	
 }
-=======
-}
->>>>>>> 0f5ed305be53fd392ca629d2a1c31dead75796b7
+
---
+++ 004/C/004.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    clock_t start = clock();
+    int a, b, c, d;
+    int n, rev = 0, temp;
+
+    for (a=100; a<=999; a++){
+        for (b=a; b<=999; b++){
+            n = a*b;    
+            temp = n;
+            while(temp != 0){
+                rev = rev * 10;
+                rev = rev + temp%10;
+                temp = temp/10; 
+            }
+                if (n==rev){
+                    c = n;
+                    if (c > d){
+                       d = c;
+                    }
+                }
+                rev = 0;  
+            }
+    }
+    printf("Largest palindrome >> %d\n", d);
+    printf("Elapsed Time >> %f seconds.\n", ((double)clock() - start) / CLOCKS_PER_SEC); 
+}
